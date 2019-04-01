df1f15da3317075d5c5129aebcfcb51c98cd75b6
Álvaro del Moral Alcántara <ama.99@hotmail.es>
2019-04-01 12:16:07 +0200

    Modificada clase principal otra vez

---
+++ HolaMundo2.java
@@ -0,0 +1,13 @@
+package holamundo2;
+
+
+public class HolaMundo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde el programa 2");
+        System.out.println("1 de abril de 2019");
+
+    }
+    
+}
+
